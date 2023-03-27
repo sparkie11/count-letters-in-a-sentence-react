f563b8c51ffd65db515a45cd20d930dbe89f6d25
sparkcodes <122539340+sparkie11@users.noreply.github.com>
2023-03-27 15:36:27 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+
+const countLetters = () => {
+  const phrase = "hello, My name is gautam.";
+
+  for (const letter in phrase) {
+    console.log(letter);
+  }
+};
+countLetters();
+function App() {
+  return <></>;
+}
+
+export default App;
